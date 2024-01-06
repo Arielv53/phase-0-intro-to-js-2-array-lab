dedef7697a5dd916b13d7f2df55c3bf085b4733e
ariel vargas <arielvs53@gmail.com>
2024-01-06 08:49:58 -0500

    array lab comp

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newArray = [...cats];
+    newArray.push("Broom");
+    return newArray;
+}
+
+function prependCat() {
+    const newArray = [...cats];
+    newArray.unshift("Arnold");
+    return newArray;
+}
+
+function removeLastCat() {
+    const newArray = [...cats];
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat() {
+    const newArray = [...cats];
+    newArray.shift();
+    return newArray;
+}
